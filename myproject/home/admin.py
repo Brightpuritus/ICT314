ae019b03fcae06716626e38b3f06be76aafc3177
Brightpuritus <brightpuritus@gmail.com>
2025-11-27 11:25:12 +0700

    last

--- myproject/home/admin.py
+++ myproject/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'icon')  # แสดงคอลัมน์ในหน้า Admin
+    search_fields = ('name',)  # เพิ่มช่องค้นหาด้วยชื่อเกม
+    list_filter = ('name',)  # เพิ่มตัวกรอง
